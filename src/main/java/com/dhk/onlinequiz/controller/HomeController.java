3a85a686069ee3c9179d0ad7ce712f85d0cae048
kimdh-hi <zbeld123@gmail.com>
2021-10-16 17:55:53 +0900

    index page

--- src/main/java/com/dhk/onlinequiz/controller/HomeController.java
+++ src/main/java/com/dhk/onlinequiz/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.dhk.onlinequiz.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -8,6 +9,27 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
+        return "index";
+    }
+
+    @GetMapping("/student/home")
+    public String studentHome(Model model) {
+        model.addAttribute("auth", "student");
+
+        return "index";
+    }
+
+    @GetMapping("/teacher/home")
+    public String teacherHome(Model model) {
+        model.addAttribute("auth", "teacher");
+
+        return "index";
+    }
+
+    @GetMapping("/admin/home")
+    public String adminHome(Model model) {
+        model.addAttribute("auth", "admin");
+
         return "index";
     }
 }
---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,10 @@
+
+function show_signin_form() {
+    console.log("toggle")
+    $("#signin-form").toggleClass("toggle_show")
+}
+
+function show_signup_form() {
+    console.log("toggle")
+    $("#signup-form").toggleClass("toggle_show")
+}
